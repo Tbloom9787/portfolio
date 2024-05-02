993c39b273fdf12f27fd2397a67f7dcbfee4ee8f
Tbloom9787 <tylerbloom9787@gmail.com>
2024-05-02 15:09:46 -0700

    Implement a clickable 'logo' to navigate back to the main section

---
+++ portfolio-frontend/src/components/common/Navbar/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo: React.FC = () => {
+  return (
+    <div className='hexagon'>
+      <span>TB</span>
+    </div>
+  );
+};
+
+export default Logo;
